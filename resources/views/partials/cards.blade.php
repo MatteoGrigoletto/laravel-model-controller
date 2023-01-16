e0b38c5ef574837cfef514aba424b8fb48190113
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2023-01-16 16:37:29 +0100

    Aggiunge card

--- resources/views/partials/cards.blade.php
+++ resources/views/partials/cards.blade.php
@@ -1,5 +1,11 @@
 <section class="cards">
-<div class="card">
-
-</div>
+	@foreach ($movies as $movie)
+  <div class="card">
+			<a href="">
+				<h4>{{$movie->title}}</h4>
+			</a>
+			<p>Nationality : {{$movie->nationality}}</p>
+			<small>Vote: {{$movie->vote}}</small>
+  </div>
+	@endforeach
 </section>
